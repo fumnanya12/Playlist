98d14887ac6c1e0ac21923799f6fe16013a15e09
Fumnanya obi <fumnanya.obi@gmail.com>
2024-08-07 18:25:02 -0500

    Save changes

---
+++ spotify_auth_app/static/login.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function() {
+    console.log('Page loaded'); // Check if the script is running
+    var loader = document.querySelector('.loader-container'); // Updated selector
+    var content = document.getElementById('content');
+    console.log('Loader:', loader);
+    console.log('Content:', content);
+    
+    if (loader && content) {
+        setTimeout(function() {
+            loader.style.display = 'none'; // Hide the loader
+            content.style.display = 'block'; // Show the content
+        }, 3000); // Delay to see the loader for 2 seconds
+    } else {
+        console.error('Loader or content element not found');
+    }
+});
