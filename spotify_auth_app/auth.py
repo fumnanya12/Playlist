d2b97521549de6af8e60a7e4128da2928380cd03
Fumnanya obi <fumnanya.obi@gmail.com>
2024-08-22 21:42:17 -0500

    heroku req

--- spotify_auth_app/auth.py
+++ spotify_auth_app/auth.py
@@ -151,9 +151,9 @@ def callback():
     global_access_token = access_token
 
     # Start the scheduler after obtaining the access token
-   
-    scheduler.add_job(store_play_job, 'interval', minutes=25)
     scheduler.start()
+    scheduler.add_job(store_play_job, 'interval', minutes=25)
+    
     return redirect(url_for('welcome'))
 
 "Welcome Page"
